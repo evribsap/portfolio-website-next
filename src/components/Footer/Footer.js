ee8a8282c9a8c2e844f80a5ea3abff3a91d6f88d
evribsap <evribsaputra@gmail.com>
2022-08-18 23:53:06 +0700

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,42 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>
+            Call
+          </LinkTitle>
+          <LinkItem href="tel:111-111-1111">
+            111-111-1111
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>
+            Email
+          </LinkTitle>
+          <LinkItem href="mailto:example@gmail.com">
+            example@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            Innovation one project at a time
+          </Slogan>
+       </CompanyContainer>
+       <SocialIcons href="https://github.com" target="_blank">
+        <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com" target="_blank">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com" target="_blank">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
